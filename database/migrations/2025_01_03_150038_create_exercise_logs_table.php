68fe2b60924738544ca79cfca70bd78478d752f5
Ruan Pablo <ruanpablodnzzz@gmail.com>
2025-01-03 12:01:03 -0300

    migrations

--- database/migrations/2025_01_03_150038_create_exercise_logs_table.php
+++ database/migrations/2025_01_03_150038_create_exercise_logs_table.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('exercise_logs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('exercise_logs');
     }
 };
